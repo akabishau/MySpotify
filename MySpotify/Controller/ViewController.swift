e3bdec7cbd9f54e5e5badcdfa1a996b4f2db66ee
AlekseyK <aleksey.kabishau@gmail.com>
2021-02-25 22:49:15 -0500

    Initial Commit

---
+++ MySpotify/Controller/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  MySpotify
+//
+//  Created by Aleksey Kabishau on 2/22/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = "Home"
+        view.backgroundColor = .systemPink
+    }
+
+
+}
+
---
+++ MySpotify/Support/AppDelegate.swift
@@ -0,0 +1,37 @@
+//
+//  AppDelegate.swift
+//  MySpotify
+//
+//  Created by Aleksey Kabishau on 2/22/21.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        window.rootViewController = ViewController()
+        window.makeKeyAndVisible()
+        self.window = window
+        
+        return true
+    }
+
+    // MARK: UISceneSession Lifecycle
+
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        // Called when a new scene session is being created.
+        // Use this method to select a configuration to create the new scene with.
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+
+    }
+}
+
