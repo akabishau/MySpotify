add1b29e161bcacf9360b5cfb1bb7ccf88180f18
AlekseyK <aleksey.kabishau@gmail.com>
2021-02-27 13:21:46 -0500

    feat: add UIView extension to help layout views in the project

---
+++ MySpotify/Support/Extensions.swift
@@ -0,0 +1,35 @@
+//
+//  Extensions.swift
+//  MySpotify
+//
+//  Created by Aleksey Kabishau on 2/27/21.
+//
+
+import UIKit
+
+extension UIView {
+    
+    var width: CGFloat {
+        return frame.size.width
+    }
+    
+    var height: CGFloat {
+        return frame.size.height
+    }
+    
+    var left: CGFloat {
+        return frame.origin.x
+    }
+    
+    var right: CGFloat {
+        return left + width
+    }
+    
+    var top: CGFloat {
+        return frame.origin.y
+    }
+    
+    var bottom: CGFloat {
+        return top + height
+    }
+}
